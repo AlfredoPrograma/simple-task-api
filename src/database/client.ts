b2c0a88ee56b9a5318993a427732db16ce36d3ff
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-06-09 21:47:16 -0400

    feature: set up api service in docker compose and connect to database service

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,3 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?schema=public`,
+    },
+  },
+});
